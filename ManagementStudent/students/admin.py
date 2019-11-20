d9d12c0f4db7c91d9f0dbdd06074615b2868adf7
dothanhnam198 <57521307+dothanhnam198@users.noreply.github.com>
2019-11-20 10:05:35 +0700

    Add files via upload

--- ManagementStudent/students/admin.py
+++ ManagementStudent/students/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Subject, Program
+from .models import Student, Subject, Program, Results, Courses, CourseRegistration, Semester
 # Register your models here.
 
 
@@ -18,5 +18,28 @@ admin.site.register(Subject, SubjectAdmin)
 class ProgramAdmin(admin.ModelAdmin):
     list_display = ['program_id', 'program_name']
     search_fields = ['program_id', 'program_name']
-
 admin.site.register(Program, ProgramAdmin)
+
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ['course_id', 'subject', 'period', 'day', 'room']
+    search_fields = ['course_id', 'subject__subject_name']
+admin.site.register(Courses, CoursesAdmin)
+
+
+class CourseRegistrationAdmin(admin.ModelAdmin):
+    list_display = ['student', 'course', 'semester', 'date_opened', 'date_closed']
+    search_fields = ['student__student_name', 'course__course_id', 'semester__semester_id']
+admin.site.register(CourseRegistration, CourseRegistrationAdmin)
+
+
+class ResultsAdmin(admin.ModelAdmin):
+    list_display = ['student', 'subject', 'semester', 'score_middle', 'score_final']
+    search_fields = ['student__student_name', 'subject__subject_name', 'semester__semester_id']
+admin.site.register(Results, ResultsAdmin)
+
+
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ['semester_id']
+    search_fields = ['semester_id']
+admin.site.register(Semester, SemesterAdmin)
--- ManagementStudent/students/models.py
+++ ManagementStudent/students/models.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
-
+User = get_user_model()
 
 class Program(models.Model):
     program_id = models.CharField(max_length=50, primary_key=True)
@@ -10,6 +11,13 @@ class Program(models.Model):
 
     def __str__(self):
         return self.program_name
+
+
+class Semester(models.Model):
+    semester_id = models.IntegerField(primary_key=True)
+
+    def __int__(self):
+        return self.semester_id
 
 
 class Subject(models.Model):
@@ -22,13 +30,42 @@ class Subject(models.Model):
         return self.subject_name
 
 
+class Courses(models.Model):
+    course_id = models.CharField(max_length=20, primary_key=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    period = models.CharField(max_length=20)
+    day = models.CharField(max_length=20)
+    room = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.course_id
+
+
 class Student(models.Model):
     student_id = models.CharField(max_length=50, primary_key=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,default=student_id, on_delete=models.CASCADE)
     student_name = models.CharField(max_length=50)
     birth_day = models.DateField(blank=True, null=True)
     grade = models.CharField(max_length=20)
     program = models.ForeignKey(Program, on_delete=models.CASCADE, blank=True, null=True)
-    subject = models.ManyToManyField(Subject, blank= True, null=True)
 
     def __str__(self):
         return self.student_name
+
+
+class CourseRegistration(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    date_opened = models.DateField()
+    date_closed = models.DateField()
+
+
+class Results(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    score_middle = models.FloatField()
+    score_final = models.FloatField()
+
+
